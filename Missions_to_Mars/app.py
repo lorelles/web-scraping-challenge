e65ebf82cb1026cd87c429f071394c9557a617c2
lorelles <74384017+lorelles@users.noreply.github.com>
2021-04-07 10:42:18 -0700

    app.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import mars_scraper
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/marsDB")
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.mars_data.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_data=mars_data)
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scraper():
+
+    # Run the scrape function
+    mars_data = mongo.db.mars_data
+    scraped_data = mars_scraper.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mars_data.update({}, scraped_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+ 
---
+++ Missions_to_Mars/mars_scraper.py
@@ -0,0 +1,66 @@
+# Import dependencies
+import os
+from bs4 import BeautifulSoup as bs
+import requests
+import pymongo
+import time
+import pandas as pd
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape_info():
+
+    # Initialize PyMongo to work with MongoDBs
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+
+    # Define the 'classDB' database in Mongo
+    db = client.marsDB
+
+    # Initialize browser
+#def init_browser():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    #executable_path = {'exeutable_path': "C:/Users/serenabaker/chromedriver_win32/chromedriver"}
+    #executable_path = {'exeutable_path': "C:/Users/serenabaker/.wdm/drivers/chromedriver/mac64/89.0.4389.23/chromedriver"}
+
+    #return Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
+    #browser = init_browser()
+
+    # URL of page to be scraped
+    news_url = "https://mars.nasa.gov/news/"
+    browser.visit(news_url)
+    # Sleep function
+    time.sleep(1)
+    
+    #response = requests.get(url)
+    
+    # Scrape page into Soup
+    #html = browser.html
+    news_html = browser.html
+    news_soup = bs(news_html, "html.parser")
+    #html = requests.get(url).text
+
+    # Scrape NASA Mars News Site and collect latest News Tiltle and Paragraph Text, assign to variables
+
+# news_title = "NASA's Curiosity Team Names Martian Hill That Serves as Mission ‘Gateway'"
+# news_p = "The name honors recently deceased mission scientist Rafael Navarro-González, who helped lead the team that identified ancient organic compounds on Mars"
+
+    news_title = news_soup.find_all('div', class_="content_title")
+    news_p = news_soup.find_all('div', class='article_teaser_body')[0].text
+
+    # Store data in dictionary
+    mars_data = {
+        "news_title": news_title,
+        "news_p": news_p
+    }
+
+print(news_title)
+print(news_p)
+print(news_soup)
+
+# Close browser after scraping
+browser.quit()
+
+return news_title
+return news_p
